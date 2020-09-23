8726ab0cf35c0b7dd098e69b063f9354963d966b
Adole Samuel <cyberorgnizm@gmail.com>
2020-09-23 01:34:45 +0100

    🚧 Build v0.1 ch2 guess_game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+// A simple guess game using random numbers
+fn guess_game() {
+    println!("Guess a number!");
+
+    println!("Please input your guess.");
+
+    let mut g = String::new();
+
+    // Panics on error!
+    io::stdin().read_line(&mut g).expect("Failed to read line!");
+
+    println!("You guessed, {}", g);
+}
+
 fn main() {
-    println!("Hello, world!");
+    guess_game();
 }

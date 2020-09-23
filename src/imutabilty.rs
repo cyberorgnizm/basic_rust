29c09e0531f096cea310c440d14b41516e79ce0a
Adole Samuel <cyberorgnizm@gmail.com>
2020-09-23 03:20:23 +0100

    🐛 Fix mutable variable

--- src/imutabilty.rs
+++ src/imutabilty.rs
@@ -1,6 +1,6 @@
 pub fn variables() {
-    // Error [🐛], variables are immutable by default
-    let x = 5;
+    // variables are immutable by default
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);

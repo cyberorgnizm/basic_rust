387437bf56ad76893afc44145ea18da629b4280f
Adole Samuel <cyberorgnizm@gmail.com>
2020-09-23 02:09:44 +0100

    🚧 Build v0.2 ch2 guess_game

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,33 @@ fn guess_game() {
     println!("Guess a number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut g = String::new();
+        let mut g = String::new();
 
-    // Panics on error!
-    // io::stdin().read_line(&mut g).expect("Failed to read line!");
+        // Panics on error!
+        // io::stdin().read_line(&mut g).expect("Failed to read line!");
 
-    // Handles panic 🐛
-    match io::stdin().read_line(&mut g) {
-        Ok(_) => println!("Your input {}", g),
-        Err(error) => println!("Error!: {}", error),
-    }
+        // Handles panic 🐛
+        match io::stdin().read_line(&mut g) {
+            Ok(_) => println!("Your input {}", g),
+            Err(error) => println!("Error!: {}", error),
+        }
 
-    println!("You guessed, {}", g);
+        println!("You guessed, {}", g);
 
-    // typecast
-    let g: u32 = g.trim().parse().expect("Number expected!");
+        // typecast
+        let g: u32 = g.trim().parse().expect("Number expected!");
 
-    match g.cmp(&secret_number) {
-        Ordering::Greater => println!("Guess value too big!"),
-        Ordering::Equal => println!("You win!!!"),
-        Ordering::Less => println!("Guess value too small!"),
+        match g.cmp(&secret_number) {
+            Ordering::Greater => println!("Guess value too big!"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+            Ordering::Less => println!("Guess value too small!"),
+        }
     }
 
     println!("Secret number is {}", secret_number);

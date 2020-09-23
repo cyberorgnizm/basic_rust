3237bc6b21a219379da75c4b663de343d5dae59a
Adole Samuel <cyberorgnizm@gmail.com>
2020-09-23 02:00:39 +0100

    ➕ Add dependency

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 // A simple guess game using random numbers
 fn guess_game() {
     println!("Guess a number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -18,6 +21,17 @@ fn guess_game() {
     }
 
     println!("You guessed, {}", g);
+
+    // typecast
+    let g: u32 = g.trim().parse().expect("Number expected!");
+
+    match g.cmp(&secret_number) {
+        Ordering::Greater => println!("Guess value too big!"),
+        Ordering::Equal => println!("You win!!!"),
+        Ordering::Less => println!("Guess value too small!"),
+    }
+
+    println!("Secret number is {}", secret_number);
 }
 
 fn main() {

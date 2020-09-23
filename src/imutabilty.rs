f132a5a5ff602e53bd91a492493b0b2efd6e80e5
Adole Samuel <cyberorgnizm@gmail.com>
2020-09-23 03:37:22 +0100

    📝 📌 Concepts (shadowing)

--- src/imutabilty.rs
+++ src/imutabilty.rs
@@ -9,4 +9,14 @@ pub fn variables() {
     const MAX_POINTS: u32 = 100_000;
 
     println!("{}", MAX_POINTS);
+
+    // Shadowing: re-delcare new variable with same name as previous one
+    let name = "John Doe";
+
+    println!("{}", name);
+
+    // effectively change the type of variable but maintain same name
+    let name = 7;
+
+    println!("{}", name)
 }
